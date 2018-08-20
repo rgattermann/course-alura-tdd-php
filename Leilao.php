7bb5c77ac4090e78b6d0880d0e787362ad23ec47
Rodrigo Luciano Gattermann <rodrigo.gattermann@topaziopartner.com.br>
2018-08-20 08:57:52 -0300

    Initial commit

---
+++ Leilao.php
@@ -0,0 +1,28 @@
+<?php
+class Leilao 
+{
+	private $descricao;
+	private $lances;
+	
+	function __construct($descricao) 
+	{
+		$this->descricao = $descricao;
+		$this->lances = [];
+	}
+	
+	public function propoe(Lance $lance) 
+	{
+		$this->lances[] = $lance;
+	}
+
+	public function getDescricao() 
+	{
+		return $this->descricao;
+	}
+
+	public function getLances() 
+	{
+		return $this->lances;
+	}
+}
+?>
